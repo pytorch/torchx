c9affc2f612d59efd1c0b73a8408696fd51a3e2b
Kiuk Chung <kiuk@fb.com>
2021-05-24 22:35:47 -0700

    register fb schedulers using entry_point
    Summary:
    NOTE: I cleaned up a few tsm references while I was at it since removing `//torchx/schedulers/fb:registry` buck target ended up with a ton of autodeps changes that touched a lot of TARGETS files. I recommend taking a look at `torchx_fb.dist-info/entry_points.txt` and `torchx/util/entrypoint.py` for the CORE changes I originally intended for this diff.
    
    1. Now that we have `torchx_fb.dist-info/entry_point.txt` setup to hook in plugin points for Facebook, move the scheduler registry to entrypoint so that we don't have to mess with `base_path` and have autodeps hate us.
    
    1. cleaned up a few places were we still have a back reference to tsm.
    
    Reviewed By: tierex
    
    Differential Revision: D28622469
    
    fbshipit-source-id: af6ec8e79865ad9714d499052f8565d0e5829a92

--- torchx/schedulers/__init__.py
+++ torchx/schedulers/__init__.py
@@ -4,3 +4,31 @@
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
+
+from typing import Dict
+
+import torchx.schedulers.local_scheduler as local_scheduler
+from torchx.schedulers.api import Scheduler
+from torchx.specs.api import SchedulerBackend
+from torchx.util.entrypoints import load_group
+
+
+def get_schedulers(
+    session_name: str,
+    # pyre-ignore[2]
+    **scheduler_params
+) -> Dict[SchedulerBackend, Scheduler]:
+
+    schedulers = load_group(
+        "torchx.schedulers",
+        default={
+            "local": local_scheduler.create_scheduler,
+            "default": local_scheduler.create_scheduler,
+        },
+        ignore_missing=True,
+    )
+
+    return {
+        scheduler_backend: scheduler_factory_method(session_name, **scheduler_params)
+        for scheduler_backend, scheduler_factory_method in schedulers.items()
+    }
