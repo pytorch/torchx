b096bb8c4a60b4ec62b61793343b7b3babb0c7c9
Aliaksandr Ivanou <aivanou@fb.com>
2021-06-09 13:11:12 -0700

    Add unit test for stl apps (#44)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchx/pull/44
    
    The diffs adds unit tests for stl apps, adds support for default values for ``fb.stl_apps.video_classification`` component. With this change users can just tests run via:
    
      torchx run --scheduler flow \
        --scheduler_args secure_group=oncall_dai_pet,entitlement=bigbasin_atn_prod \
        fb.stl_apps.video_classification
    
    Reviewed By: d4l3k
    
    Differential Revision: D28983911
    
    fbshipit-source-id: 1d44e47889d141aa701a6a5d10161aff9088d024

---
+++ torchx/components/test/component_test_base.py
@@ -0,0 +1,25 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import os
+import unittest
+from types import ModuleType
+
+from torchx.specs.file_linter import validate
+
+
+class ComponentTestCase(unittest.TestCase):
+    def _validate(self, module: ModuleType, function_name: str) -> None:
+        file_path = path = os.path.abspath(module.__file__)
+        with open(file_path, "r") as fp:
+            file_content = fp.read()
+        linter_errors = validate(file_content, file_path, function_name)
+        if len(linter_errors) != 0:
+            error_msg = ""
+            for linter_error in linter_errors:
+                error_msg += f"Lint Error: {linter_error.description}\n"
+            raise ValueError(error_msg)
+        self.assertEquals(0, len(linter_errors))
--- torchx/components/test/distributed_test.py
+++ torchx/components/test/distributed_test.py
@@ -4,21 +4,11 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import os
-import unittest
-from types import ModuleType
-
 import torchx.components.distributed as distributed_components
-from torchx.specs.file_linter import validate
+
+from .component_test_base import ComponentTestCase
 
 
-class DistributedComponentTest(unittest.TestCase):
-    def _validate(self, module: ModuleType, function_name: str) -> None:
-        file_path = path = os.path.abspath(distributed_components.__file__)
-        with open(file_path, "r") as fp:
-            file_content = fp.read()
-        linter_errors = validate(file_content, file_path, function_name)
-        self.assertEquals(0, len(linter_errors))
-
+class DistributedComponentTest(ComponentTestCase):
     def test_ddp(self) -> None:
         self._validate(distributed_components, "ddp")
