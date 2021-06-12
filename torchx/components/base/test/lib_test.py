fc3c33d3bd68fc316aa0771b654853b2069929fa
Aliaksandr Ivanou <aivanou@fb.com>
2021-06-11 18:29:24 -0700

    Add named resource representation (#52)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchx/pull/52
    
    The diff adds ``torchx.specs.named_resource`` method that allows resolution of string resources to the ``torchx.specs.api.Resource`` object.
    
    Named resources can be configured via ``entrypoints.txt`` in format:
    
        [torchx.resource]
        p3_xlarge = custom_module.resource:get_p3_xlarge
    
    Reviewed By: kiukchung
    
    Differential Revision: D29012120
    
    fbshipit-source-id: 2f0be0095424e73620642648b3afbf7b07f35072

---
+++ torchx/components/base/test/lib_test.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import unittest
+from importlib import metadata
+from unittest.mock import patch
+
+from torchx.components.base import torch_dist_role
+from torchx.specs.api import Resource
+
+
+class TorchxBaseLibTest(unittest.TestCase):
+    def test_torch_dist_role_str_resources(self) -> None:
+        expected_resources = {}
+        with patch.object(metadata, "entry_points") as entry_points_mock:
+            entry_points_mock.return_value = {}
+            with self.assertRaises(ValueError):
+                torch_dist_role(
+                    name="dist_role",
+                    image="test_image",
+                    entrypoint="test_entry.py",
+                    base_image="test_base_image",
+                    resource="unknown resource",
+                )
+
+    def test_torch_dist_role_default(self) -> None:
+        with patch.object(metadata, "entry_points") as entry_points_mock:
+            entry_points_mock.return_value = {}
+            role = torch_dist_role(
+                name="dist_role",
+                image="test_image",
+                entrypoint="test_entry.py",
+                base_image="test_base_image",
+                resource=Resource(1, 1, 10),
+                script_args=["arg1", "arg2"],
+                script_envs={"FOO": "BAR"},
+                nnodes=2,
+            )
+
+        self.assertEqual("python", role.entrypoint)
+        expected_args = [
+            "-m",
+            "torch.distributed.launch",
+            "--nnodes",
+            "2",
+            "--rdzv_backend",
+            "etcd",
+            "--rdzv_id",
+            "${app_id}",
+            "--role",
+            "dist_role",
+            "${img_root}/test_entry.py",
+            "arg1",
+            "arg2",
+        ]
+        self.assertListEqual(expected_args, role.args)
--- torchx/specs/__init__.py
+++ torchx/specs/__init__.py
@@ -6,3 +6,17 @@
 # LICENSE file in the root directory of this source tree.
 
 from .api import *  # noqa: F401 F403
+from torchx.util.entrypoints import load
+
+
+def named_resource(resource: str) -> Resource:
+    """
+    Gets resource object based on the string definition registered via entrypoints.txt.
+    """
+    try:
+        resource_fn = load("torchx.resources", name=resource.lower())
+    except KeyError:
+        raise ValueError(
+            f"Resource: {resource} is not registered, check entrypoints.txt file."
+        )
+    return resource_fn()
