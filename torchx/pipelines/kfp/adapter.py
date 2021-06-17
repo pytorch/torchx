9d3583cc48f045d6ff5c44e5e9cd18d1c3969bb3
Tristan Rice <rice@fn.lc>
2021-06-17 12:57:17 -0700

    cleanup old Component AP

--- torchx/pipelines/kfp/adapter.py
+++ torchx/pipelines/kfp/adapter.py
@@ -5,127 +5,14 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import copy
-import os
-from typing import Callable, Dict, List, Optional, Protocol, Tuple, Type
+from typing import Protocol, Tuple
 
 import yaml
 from kfp import components, dsl
 from kubernetes.client.models import V1ContainerPort
-from torchx.runtime.component import Component, is_optional
 from torchx.specs import api
 
 from .version import __version__ as __version__  # noqa F401
-
-
-TORCHX_CONTAINER_ENV: str = "TORCHX_CONTAINER"
-TORCHX_CONTAINER: str = os.getenv(
-    TORCHX_CONTAINER_ENV,
-    "pytorch/torchx:latest",
-)
-TORCHX_ENTRY_POINT = "torchx/container/main.py"
-PYTHON_COMMAND = "python3"
-
-
-# pyre-fixme[24]: Generic type `Component` expects 3 type parameters.
-def component_spec(c: Type[Component], image: Optional[str] = None) -> str:
-    """
-    deprecated: do not use
-    """
-
-    assert issubclass(c, Component), f"{c} must be a subclass of Component"
-    inputs = []
-    outputs = []
-    qualname = f"{c.__module__}.{c.__qualname__}"
-
-    command: List[object] = [
-        PYTHON_COMMAND,
-        TORCHX_ENTRY_POINT,
-        qualname,
-    ]
-
-    Config, Inputs, Outputs = c._get_args()
-    for arg in (Config, Inputs, Outputs):
-        for fieldname, fieldtype in arg.__annotations__.items():
-            inp = {"name": fieldname, "type": "String"}
-            if is_optional(fieldtype):
-                inp["default"] = "null"
-            inputs.append(inp)
-            command += [
-                f"--{fieldname}",
-                {"inputValue": fieldname},
-            ]
-            if arg == Outputs:
-                outputs.append(copy.deepcopy(inp))
-                command += [
-                    f"--output-path-{fieldname}",
-                    {"outputPath": fieldname},
-                ]
-
-    spec = {
-        "name": c.__name__,
-        "description": f"KFP wrapper for TorchX component {qualname}. Version: {c.Version}",
-        "inputs": inputs,
-        "outputs": outputs,
-        "implementation": {
-            "container": {
-                "image": image or TORCHX_CONTAINER,
-                "command": command,
-            }
-        },
-    }
-
-    return yaml.dump(spec)
-
-
-# pyre-fixme[24]: Generic type `Component` expects 3 type parameters.
-# pyre-fixme[24]: Generic type `Callable` expects 2 type parameters.
-def component_op(c: Type[Component], image: Optional[str] = None) -> Callable:
-    """
-    deprecated: do not use
-    """
-
-    spec = component_spec(c, image=image)
-    return components.load_component_from_text(spec)
-
-
-class TorchXComponent:
-    """
-    deprecated: do not use
-    """
-
-    # pyre-fixme[24]: Generic type `Callable` expects 2 type parameters.
-    _factory: Optional[Callable] = None
-
-    def __init_subclass__(
-        cls,
-        *args: object,
-        # pyre-fixme[24]: Generic type `Component` expects 3 type parameters.
-        component: Optional[Type[Component]] = None,
-        image: Optional[str] = None,
-        **kwargs: object,
-    ) -> None:
-        assert component and issubclass(
-            component, Component
-        ), f"must specify component, got {component}"
-        cls._factory = component_op(component, image=image)
-
-        super().__init_subclass__(*args, **kwargs)
-
-    def __new__(cls, *args: object, **kwargs: object) -> "TorchXComponent":
-        factory = cls._factory
-        assert factory, "must have component"
-        return factory(*args, **kwargs)
-
-    # These methods are never run since we override the __new__ method but it gives us type checking.
-
-    @property
-    def outputs(self) -> Dict[str, dsl.PipelineParam]:
-        ...
-
-    @property
-    def output(self) -> dsl.PipelineParam:
-        ...
 
 
 def component_spec_from_app(app: api.AppDef) -> Tuple[str, api.Role]:
--- torchx/pipelines/kfp/test/adapter_test.py
+++ torchx/pipelines/kfp/test/adapter_test.py
@@ -8,108 +8,15 @@
 import os.path
 import tempfile
 import unittest
-from typing import Callable, List, Optional, TypedDict
+from typing import Callable, List
 
 from kfp import compiler, components, dsl
 from kubernetes.client.models import V1ContainerPort, V1ResourceRequirements
-from torchx.apps.io.copy import Copy
 from torchx.pipelines.kfp.adapter import (
-    TorchXComponent,
     component_from_app,
-    component_spec,
     component_spec_from_app,
 )
-from torchx.runtime.component import Component
 from torchx.specs import api
-
-
-class Config(TypedDict):
-    a: int
-    b: Optional[int]
-
-
-class Inputs(TypedDict):
-    input_path: str
-
-
-class Outputs(TypedDict):
-    output_path: str
-
-
-class TestComponent(Component[Config, Inputs, Outputs]):
-    Version: str = "0.1"
-
-    def run(self, inputs: Inputs, outputs: Outputs) -> None:
-        ...
-
-
-class KFPTest(unittest.TestCase):
-    def test_component_spec(self) -> None:
-        self.maxDiff = None
-        spec = component_spec(TestComponent)
-        self.assertIsNotNone(components.load_component_from_text(spec))
-        self.assertEqual(
-            spec,
-            """description: 'KFP wrapper for TorchX component torchx.pipelines.kfp.test.adapter_test.TestComponent.
-  Version: 0.1'
-implementation:
-  container:
-    command:
-    - python3
-    - torchx/container/main.py
-    - torchx.pipelines.kfp.test.adapter_test.TestComponent
-    - --a
-    - inputValue: a
-    - --b
-    - inputValue: b
-    - --input_path
-    - inputValue: input_path
-    - --output_path
-    - inputValue: output_path
-    - --output-path-output_path
-    - outputPath: output_path
-    image: pytorch/torchx:latest
-inputs:
-- name: a
-  type: String
-- default: 'null'
-  name: b
-  type: String
-- name: input_path
-  type: String
-- name: output_path
-  type: String
-name: TestComponent
-outputs:
-- name: output_path
-  type: String
-""",
-        )
-
-    def test_pipeline(self) -> None:
-        class KFPCopy(TorchXComponent, component=Copy):
-            pass
-
-        def pipeline() -> dsl.PipelineParam:
-            a = KFPCopy(
-                input_path="file:///etc/os-release", output_path="file:///tmp/foo"
-            )
-            b = KFPCopy(
-                input_path=a.outputs["output_path"], output_path="file:///tmp/bar"
-            )
-            return b.output
-
-        with tempfile.TemporaryDirectory() as tmpdir:
-            compiler.Compiler().compile(pipeline, os.path.join(tmpdir, "pipeline.zip"))
-
-    def test_image(self) -> None:
-        class KFPCopy(TorchXComponent, component=Copy, image="foo"):
-            pass
-
-        copy = KFPCopy(input_path="", output_path="")
-        print(copy)
-        # pyre-fixme[16]: `KFPCopy` has no attribute `component_ref`.
-        self.assertEqual(copy.component_ref.spec.implementation.container.image, "foo")
 
 
 class KFPSpecsTest(unittest.TestCase):
