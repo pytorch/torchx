7ea3795782e5f34dc0c92e77b1ef404a3a792510
Aliaksandr Ivanou <aivanou@fb.com>
2021-06-21 16:53:08 -0700

    Flow workflow based on torchx (#66)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchx/pull/66
    
    The diff adds example of building flow workflow using torchx. The job consists of several separate parts:
    
    1. Applications. (Datapreproc and cv trainer). Pure python code
    2. Components definition. Returns definition of the components.
    3. Dag definition. (workflow.py) .
    
    See ``fblearner/flow/projects/torchx/examples/workflow.py`` for E2E example
    
    Reviewed By: d4l3k
    
    Differential Revision: D28839510
    
    fbshipit-source-id: cb38254d8b1908f094c481be942c594af8fa7a12

--- examples/apps/lightning_classy_vision/component.py
+++ examples/apps/lightning_classy_vision/component.py
@@ -11,20 +11,25 @@ Trainer Component Example
 This is a component definition that runs the example lightning_classy_vision app.
 """
 
-from typing import Optional
+from typing import Optional, Dict
 
 import torchx.specs.api as torchx
-from torchx.components.base import named_resources
 from torchx.components.base.binary_component import binary_component
+from torchx.specs import named_resources
 
 
 def trainer(
     image: str,
     output_path: str,
     data_path: str,
+    entrypoint: str = "lightning_classy_vision/train.py",
     load_path: str = "",
     log_path: str = "/logs",
     resource: Optional[str] = None,
+    nnodes: int = 1,
+    env: Optional[Dict[str, str]] = None,
+    nproc_per_node: int = 1,
+    skip_export: bool = False,
 ) -> torchx.AppDef:
     """Runs the example lightning_classy_vision app.
 
@@ -35,20 +40,29 @@ def trainer(
         data_path: path to the data to load
         log_path: path to save tensorboard logs to
         resource: the resources to use
+        nnodes: number of nodes
+        env: env variables for the app
+        nproc_per_node: number of processes per node
+        skip_export: disable model export
     """
+    env = env or {}
+    args = [
+        "--output_path",
+        output_path,
+        "--load_path",
+        load_path,
+        "--log_pat",
+        log_path,
+        "--data_path",
+        data_path,
+    ]
+    if skip_export:
+        args.append("--skip_export")
     return binary_component(
         name="examples-lightning_classy_vision-trainer",
-        entrypoint="lightning_classy_vision/train.py",
-        args=[
-            "--output_path",
-            output_path,
-            "--load_path",
-            load_path,
-            "--log_pat",
-            log_path,
-            "--data_path",
-            data_path,
-        ],
+        entrypoint=entrypoint,
+        args=args,
+        env=env,
         image=image,
         resource=named_resources[resource]
         if resource
