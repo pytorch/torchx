bbfca3329bd44ea8f40f125d81f7c7c4ee198acd
Aliaksandr Ivanou <aivanou@fb.com>
2021-08-24 14:09:39 -0700

    Add dummy script

--- scripts/kube_dist_trainer.py
+++ scripts/kube_dist_trainer.py
@@ -11,7 +11,28 @@
 """
 
 from torchx.runner import get_runner
+from torchx.specs import Resource
+from torchx.specs import named_resources
+
+GiB: int = 1024
+
+
+def p3_2xlarge() -> Resource:
+    return Resource(
+        # TODO(aivanou): Determine why the number of CPUs allowed to
+        # be requested via volcano is N-1
+        cpu=7,
+        gpu=1,
+        memMB=61 * GiB,
+    )
+
+
+def register_gpu_resource() -> None:
+    res = p3_2xlarge()
+    print(f"Registering resource: {res}")
+    named_resources["p3_2xlarge"] = res
+
 
 if __name__ == "__main__":
-    runner = get_runner()
-    print(f"Got runner: {runner}")
+    register_gpu_resource()
+    runner = get_runner("kubernetes")
