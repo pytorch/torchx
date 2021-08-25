2e8784b256e014dba200b5dc9c4b0d8f336d95f9
Aliaksandr Ivanou <aivanou@fb.com>
2021-08-25 08:59:02 -0700

    Add dummy script

--- scripts/kube_dist_trainer.py
+++ scripts/kube_dist_trainer.py
@@ -11,8 +11,7 @@
 """
 
 from torchx.runner import get_runner
-from torchx.specs import Resource
-from torchx.specs import named_resources
+from torchx.specs import Resource, named_resources, RunConfig
 from examples.apps.dist_cifar.component import trainer
 
 GiB: int = 1024
@@ -37,3 +36,19 @@ def register_gpu_resource() -> None:
 if __name__ == "__main__":
     register_gpu_resource()
     runner = get_runner("kubernetes")
+    args = ("--output_path", "/tmp")
+    img = "public.ecr.aws/y5x3w0a7/aivanou-tests"
+    train_app = trainer(
+        *args,
+        image=img,
+        resource="p3_2xlarge",
+        nnodes=2,
+        rdzv_backend="etcd-v2",
+        rdzv_endpoint="etcd-server:2379"
+    )
+    cfg = RunConfig()
+    cfg.set("namespace", "default")
+    cfg.set("queue", "test")
+    app_handle = runner.run(train_app, cfg)
+    print(app_handle)
+    runner.wait(app_handle)
