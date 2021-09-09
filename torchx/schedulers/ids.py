3f2d8b6b100cb42a4bbcc3635359f32e93d67c68
Tristan Rice <rice@fn.lc>
2021-09-09 16:43:58 -0700

    schedulers: improved ID generation to conform with k8s requirements (#170)
    Summary:
    * switches generated ids to be `[a-z][a-z0-9]*` to match valid k8s IDs -- not strictly needed since we currently only append the IDs but it's a bit shorter
    * Fixes broken integration tests: https://github.com/pytorch/torchx/runs/3550036832
    * cleans up some ID usage
    * fixes kubernetes scheduler import path for ApiException to work with `kubernetes==11`
    
    Pull Request resolved: https://github.com/pytorch/torchx/pull/170
    
    Test Plan:
    kfp + k8s CI
    
    pyre
    
    Reviewed By: aivanou
    
    Differential Revision: D30850175
    
    Pulled By: d4l3k
    
    fbshipit-source-id: 61b7aeff61d6032b18a5dac383d4a26503e5ca7d

--- torchx/schedulers/ids.py
+++ torchx/schedulers/ids.py
@@ -5,16 +5,45 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import binascii
 import os
+import struct
 
 
 def make_unique(name: str) -> str:
     """
-    Appends the unique 64-bit hex string to the input argument.
+    Appends a unique 64-bit string to the input argument.
 
     Returns:
-        string in format $name_$unique_suffix
+        string in format $name-$unique_suffix
     """
-    rand_suffix = binascii.b2a_hex(os.urandom(8)).decode("utf-8")
-    return f"{name}_{rand_suffix}"
+    rand_suffix = random_id()
+    return f"{name}-{rand_suffix}"
+
+
+def random_uint64() -> int:
+    """
+    random_uint64 returns an random unsigned 64 bit int.
+    """
+    return struct.unpack("!Q", os.urandom(8))[0]
+
+
+def random_id() -> str:
+    """
+    Generates an alphanumeric string ID that matches the requirements from
+    https://kubernetes.io/docs/concepts/overview/working-with-objects/names/
+    """
+    START_CANDIDATES = "abcdefghijklmnopqrstuvwxyz"
+    END_CANDIDATES = START_CANDIDATES + "012345679"
+
+    out = ""
+    v = random_uint64()
+    while v > 0:
+        if out == "":
+            candidates = START_CANDIDATES
+        else:
+            candidates = END_CANDIDATES
+
+        char = v % len(candidates)
+        v = v // len(candidates)
+        out += candidates[char]
+    return out
