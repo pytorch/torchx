0f925710cc29cb64905b36d73565d45b863f01b0
Tristan Rice <rice@fn.lc>
2021-09-15 15:19:13 -0700

    workflows/nightly: add nightly builds

---
+++ scripts/push_nightly.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+set -ex
+
+rm -r dist || true
+
+
+python setup.py --override-name torchx-nightly bdist_wheel
+
+if [ -z "$PYPI_TOKEN" ]; then
+    echo "must specify PYPI_TOKEN"
+    exit 1
+fi
+
+python3 -m twine upload \
+    --username __token__ \
+    --password "$PYPI_TOKEN" \
+    dist/torchx_nightly-*
