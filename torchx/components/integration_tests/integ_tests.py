f5ca911dc93914e6f8dbf97bbde05c70dcdef14a
Aliaksandr Ivanou <aivanou@fb.com>
2021-09-20 18:11:08 -0700

    test run

---
+++ torchx/components/integration_tests/integ_tests.py
@@ -0,0 +1,140 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from concurrent.futures import Future, ThreadPoolExecutor
+import os
+from typing import List, Dict, Optional
+from dataclasses import dataclass
+
+import examples.apps.datapreproc.component as dp_component
+import examples.apps.dist_cifar.component as dist_cifar_component
+import examples.apps.lightning_classy_vision.component as cv_component
+import torchx.components.utils as utils_components
+from torchx.components.component_test_base import ComponentTestCase
+from torchx.specs import RunConfig, AppDef, SchedulerBackend
+
+
+@dataclass
+class AppDefRun:
+    app_name: str
+    scheduler: str
+    future: Future
+
+
+class IntegComponentTest:
+    def __init__(self) -> None:
+        self.component_test_case = ComponentTestCase()
+        self._schedulers_args: Dict[str, RunConfig] = {
+            "local": self._get_torchx_local_args(),
+            "kubernetes": self._get_torchx_k8s_args(),
+        }
+
+    def run_builtin_components(
+            self, image: str, schedulers: List[SchedulerBackend], dryrun: bool = False
+    ) -> None:
+        with ThreadPoolExecutor() as executor:
+            futures: List[AppDefRun] = []
+            for scheduler in schedulers:
+                app_defs = AppDefProvider().get_app_defs(image, scheduler)
+                for app_def in app_defs:
+                    future = self.run_appdef_on_scheduler(executor, app_def, scheduler, dryrun)
+                    futures.append(AppDefRun(app_def.name, scheduler, future))
+            report = self._wait_and_construct_report(futures)
+            print(report)
+
+    def _wait_and_construct_report(self, futures: List[AppDefRun], timeout: int = 600) -> str:
+        return "\n".join(self._get_app_def_run_status(app_def_run) for app_def_run in futures)
+
+    def _get_app_def_run_status(self, app_def_run: AppDefRun) -> str:
+        try:
+            app_def_run.future.result()
+            return f"`{app_def_run.app_name}`:`{app_def_run.scheduler}` succeeded"
+        except Exception as e:
+            return f"`{app_def_run.app_name}`:`{app_def_run.scheduler}` failed with error: {e}"
+
+    def run_appdef_on_scheduler(
+            self, executor: ThreadPoolExecutor, app_def: AppDef, scheduler: SchedulerBackend,
+            dryrun: bool = False
+    ) -> Future:
+        scheduler_args = self._schedulers_args.get(scheduler)
+        if not scheduler_args:
+            raise ValueError(f"Unknown scheduler: {scheduler}")
+        return executor.submit(
+            self.component_test_case.run_appdef_on_scheduler,
+            app_def=app_def,
+            scheduler=scheduler,
+            scheduler_cfg=scheduler_args,
+            dryrun=dryrun,
+        )
+
+    def _get_torchx_k8s_args(self) -> RunConfig:
+        cfg = RunConfig()
+        cfg.set("namespace", "torchx-dev")
+        cfg.set("queue", "default")
+        return cfg
+
+    def _get_torchx_local_args(self) -> RunConfig:
+        cfg = RunConfig()
+        cfg.set("image_type", "docker")
+        return cfg
+
+
+class AppDefProvider:
+    def get_app_defs(self, image: str, scheduler: SchedulerBackend) -> List[AppDef]:
+        app_defs = [
+            self._get_cv_app_def(image, scheduler),
+            self._get_dp_app_def(image, scheduler),
+            self._get_dist_cifar_app_def(image, scheduler),
+            self._get_booth_app_def(image, scheduler),
+            self._get_echo_app_def(image, scheduler),
+            self._get_sh_app_def(image, scheduler),
+        ]
+
+        return [app_def for app_def in app_defs if app_def]
+
+    def _get_cv_app_def(
+            self, image: str, scheduler: SchedulerBackend
+    ) -> Optional[AppDef]:
+        return cv_component.trainer(
+            image=image,
+            output_path="/tmp",
+            skip_export=True,
+            log_path="/tmp",
+        )
+
+    def _get_dist_cifar_app_def(
+            self, image: str, scheduler: SchedulerBackend
+    ) -> Optional[AppDef]:
+        args = ["--output_path", "/tmp", "--dryrun"]
+        return dist_cifar_component.trainer(
+            *args,
+            image=image,
+        )
+
+    def _get_dp_app_def(
+            self, image: str, scheduler: SchedulerBackend
+    ) -> Optional[AppDef]:
+        return dp_component.data_preproc(
+            image=image,
+            output_path="/tmp",
+            dryrun=True,
+        )
+
+    def _get_echo_app_def(
+            self, image: str, scheduler: SchedulerBackend
+    ) -> Optional[AppDef]:
+        return utils_components.echo()
+
+    def _get_sh_app_def(
+            self, image: str, scheduler: SchedulerBackend
+    ) -> Optional[AppDef]:
+        return utils_components.sh("echo", "test")
+
+    def _get_booth_app_def(
+            self, image: str, scheduler: SchedulerBackend
+    ) -> Optional[AppDef]:
+        return utils_components.booth(1, 2)
