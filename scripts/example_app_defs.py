6d1dcef025aaf81dca5d2f4e03cc2be9cc451fe5
Aliaksandr Ivanou <aivanou@fb.com>
2021-09-20 18:49:51 -0700

    test run

---
+++ scripts/example_app_defs.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""
+App Defs for integration tests.
+"""
+
+from typing import Optional, List
+
+import examples.apps.dist_cifar.component as dist_cifar_component
+import examples.apps.lightning_classy_vision.component as cv_component
+import examples.apps.datapreproc.component as dp_component
+
+from torchx.specs import AppDef, SchedulerBackend
+
+
+def get_example_app_defs(image: str, scheduler: SchedulerBackend) -> List[AppDef]:
+    app_defs: List[AppDef] = [
+        _get_cv_app_def(image, scheduler),
+        _get_dist_cifar_app_def(image, scheduler),
+        _get_dp_app_def(image, scheduler),
+    ]
+    return [app_def for app_def in app_defs if app_def]
+
+
+def _get_cv_app_def(
+        image: str, scheduler: SchedulerBackend
+) -> Optional[AppDef]:
+    return cv_component.trainer(
+        image=image,
+        output_path="/tmp",
+        skip_export=True,
+        log_path="/tmp",
+    )
+
+
+def _get_dist_cifar_app_def(
+        image: str, scheduler: SchedulerBackend
+) -> Optional[AppDef]:
+    args = ["--output_path", "/tmp", "--dryrun"]
+    return dist_cifar_component.trainer(
+        *args,
+        image=image,
+    )
+
+
+def _get_dp_app_def(
+        image: str, scheduler: SchedulerBackend
+) -> Optional[AppDef]:
+    return dp_component.data_preproc(
+        image=image,
+        output_path="/tmp",
+        dryrun=True,
+    )
