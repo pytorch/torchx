228fcff39c27df9de5c94816d4be6ea85730adb4
Kiuk Chung <kiuk@fb.com>
2021-09-29 17:58:07 -0700

    (torchx/runtime/hpo) fix ax_test for fb, cleaned up apps/* and added prepend_cwd runopt to local_scheduler + some local scheduler code cleanup (#210)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torchx/pull/210
    
    1. Makes ax_test.py work again in sandcastle
    2. Clean up `apps/*` directory of unused/empty placeholder modules
    3. Add `//torchx/apps:penv` python_binary to be able to deploy apps as penv
    4. Add `prepend_cwd` runopt for local scheduler to prepend CWD to replica PATH so that binaries in CWD can take precedence over the ones in PATH (needed to enable ax_test in fb)
    5. Some syntactical cleanup for local_scheduler
    6. (minimally) deletes base_image (yes it doesn't delete all of it, I'm trying to keep the diff as relevant to enabling ax_test, but had to remove some of it to simplify local_scheduler logic)
    
    Reviewed By: aivanou
    
    Differential Revision: D31262463
    
    fbshipit-source-id: 3f1649e4bc062f3fe6b54f13b03d11b53164fc8d

--- torchx/apps/__init__.py
+++ torchx/apps/__init__.py
@@ -4,7 +4,3 @@
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
-
-from .data import *  # noqa F401
-from .train import *  # noqa F401
-from .hpo import *  # noqa F401
--- torchx/runtime/hpo/test/ax_test.py
+++ torchx/runtime/hpo/test/ax_test.py
@@ -66,7 +66,7 @@ class TorchXSchedulerTest(unittest.TestCase):
             tracker_base=self.test_dir,
             component=utils.booth,
             scheduler="local_cwd",
-            scheduler_args=RunConfig({"log_dir": self.test_dir}),
+            scheduler_args=RunConfig({"log_dir": self.test_dir, "prepend_cwd": True}),
         )
 
     def tearDown(self) -> None:
