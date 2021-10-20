pytorch/torchx

commit 88a3f41d3fab5eb22d10b00ac3546c37dd20dd8e

schedulers/local_scheduler: add nice error if docker is not present (#284)
Summary:
This changes the local_docker scheduler to print a nicer error if docker is not installed.

Pull Request resolved: https://github.com/pytorch/torchx/pull/284

Test Plan: ![2021-10-19-161930_808x469_scrot](https://user-images.githubusercontent.com/909104/138003504-06a63f99-3b93-47af-a736-d9fa72bb8611.png)

Reviewed By: kiukchung

Differential Revision: D31779801

Pulled By: d4l3k

fbshipit-source-id: 2deea34bd0c0e7c2481f07b5c9f35c7c216a1f21

torchx/schedulers/local_scheduler.py
@@ -225,9 +225,29 @@ class DockerImageProvider(ImageProvider):
        ``get_command(image="pytorch/pytorch:latest", ..)`` return ``docker run ...``"""
 
     def __init__(self, cfg: RunConfig) -> None:
-        pass
+        self._has_docker: Optional[bool] = None
+
+    @staticmethod
+    def has_docker() -> bool:
+        try:
+            return subprocess.run(["docker", "version"]).returncode == 0
+        except FileNotFoundError:
+            return False
+
+    def _assert_docker(self) -> None:
+        if self._has_docker is None:
+            self._has_docker = self.has_docker()
+
+        if not self._has_docker:
+            raise FileNotFoundError(
+                """Docker is required to use the local_docker scheduler.
+
+Please see https://docs.docker.com/get-docker/ for information on how to install it on your local system."""
+            )
 
     def fetch(self, image: str) -> str:
+        self._assert_docker()
+
         try:
             subprocess.run(["docker", "pull", image], check=True)
         except Exception as e:

torchx/schedulers/test/local_scheduler_test.py
@@ -11,13 +11,13 @@ import multiprocessing as mp
 import os
 import shutil
 import signal
-import subprocess
 import tempfile
 import time
 import unittest
+from contextlib import contextmanager
 from datetime import datetime
 from os.path import join
-from typing import Optional
+from typing import Optional, Generator
 from unittest import mock
 from unittest.mock import MagicMock, call, patch
 
@@ -170,8 +170,11 @@ class CWDImageProviderTest(unittest.TestCase):
 
 
 class DockerImageProviderTest(unittest.TestCase):
+    @patch("torchx.schedulers.local_scheduler.DockerImageProvider.has_docker")
     @patch("subprocess.run")
-    def test_fetch(self, run: MagicMock) -> None:
+    def test_fetch(self, run: MagicMock, has_docker: MagicMock) -> None:
+        has_docker.return_value = True
+
         cfg = RunConfig()
         provider = DockerImageProvider(cfg)
         img = "pytorch/pytorch:latest"
@@ -886,14 +889,17 @@ class JoinPATHTest(unittest.TestCase):
         )
 
 
-def _has_docker() -> bool:
+@contextmanager
+def _temp_setenv(k: str, v: str) -> Generator[None, None, None]:
+    old = os.environ[k]
+    os.environ[k] = v
     try:
-        return subprocess.run(["docker", "version"]).returncode == 0
-    except FileNotFoundError:
-        return False
+        yield
+    finally:
+        os.environ[k] = old
 
 
-if _has_docker():
+if DockerImageProvider.has_docker():
 
     class LocalDockerSchedulerTest(unittest.TestCase, LocalSchedulerTestUtil):
         def setUp(self) -> None:
@@ -932,3 +938,13 @@ if _has_docker():
             desc = self.wait(app_id)
             assert desc is not None
             self.assertEqual(AppState.FAILED, desc.state)
+
+        def test_no_docker(self) -> None:
+            with _temp_setenv("PATH", ""):
+                with self.assertRaisesRegex(
+                    FileNotFoundError,
+                    "Docker is required to use the local_docker scheduler.",
+                ):
+                    app = self._docker_app("echo", "foo")
+                    cfg = RunConfig({"image_type": "docker"})
+                    app_id = self.scheduler.submit(app, cfg)

commit deafd417e3322e82315a3fc9ba214353b9ed1a20

schedulers/local_scheduler: silence docker version output (#290)
Summary:
https://github.com/pytorch/torchx/commit/88a3f41d3fab5eb22d10b00ac3546c37dd20dd8e accidentally added some extra log spam from `docker version` this hides it.

Pull Request resolved: https://github.com/pytorch/torchx/pull/290

Test Plan:
```
tristanr@tristanr-arch2 ~/D/torchx (dockerquiet)> torchx run  utils.echo
0.1.0rc2: Pulling from pytorch/torchx
Digest: sha256:f97df7bd3d2137b3dcb6b85e14f4eef9c1c7cdd826d12508cc7cafb08bb2f704
Status: Image is up to date for ghcr.io/pytorch/torchx:0.1.0rc2
ghcr.io/pytorch/torchx:0.1.0rc2
local_docker://torchx/echo_484dcf48
torchx 2021-10-20 14:23:25 INFO     Waiting for the app to finish...
WARNING: IPv4 forwarding is disabled. Networking will not work.
hello world
torchx 2021-10-20 14:23:27 INFO     Job finished: SUCCEEDED
```

Reviewed By: aivanou

Differential Revision: D31809970

Pulled By: d4l3k

fbshipit-source-id: cac5076e8cf9f5ca691fdefe1a31c07fe17b1e81

torchx/schedulers/local_scheduler.py
@@ -230,7 +230,14 @@ class DockerImageProvider(ImageProvider):
     @staticmethod
     def has_docker() -> bool:
         try:
-            return subprocess.run(["docker", "version"]).returncode == 0
+            return (
+                subprocess.run(
+                    ["docker", "version"],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL,
+                ).returncode
+                == 0
+            )
         except FileNotFoundError:
             return False
 