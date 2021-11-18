a9c1a3db0b1b2a0027be62809443462197031452
Tristan Rice <rice@fn.lc>
2021-11-18 13:44:38 -0800

    torchx/notebook: added workspacefile magic for use with notebooks (#356)
    Summary:
    This adds the notebook workspace magic/helpers as described in https://github.com/pytorch/torchx/issues/344
    
    Pull Request resolved: https://github.com/pytorch/torchx/pull/356
    
    Test Plan:
    pytest torchx/test/notebook_test.py
    pyre
    
    Reviewed By: aivanou
    
    Differential Revision: D32519721
    
    Pulled By: d4l3k
    
    fbshipit-source-id: a398ab048a047b1baacd259b7f2dedd82d2c39de

---
+++ torchx/notebook.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""
+This contains TorchX utilities for creating and running components and apps from
+an Jupyter/IPython Notebook.
+"""
+
+import posixpath
+
+import fsspec
+from IPython.core.magic import register_cell_magic
+
+
+def get_workspace() -> str:
+    """
+    get_workspace returns the TorchX notebook workspace fsspec path.
+    """
+    return "memory://torchx-workspace/"
+
+
+@register_cell_magic
+def workspacefile(line: str, cell: str) -> None:
+    workspace = get_workspace()
+    fs, path = fsspec.core.url_to_fs(workspace)
+    path = posixpath.join(path, line)
+
+    base = posixpath.dirname(path)
+    if not fs.exists(base):
+        fs.mkdirs(base, exist_ok=True)
+
+    with fs.open(path, "wt") as f:
+        f.write(cell)
+
+    print(f"Added {line} to workspace {workspace}")
