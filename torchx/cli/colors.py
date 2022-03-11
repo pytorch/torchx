94c16b0e4839188556b5e24ff45c14aafab46308
Tristan Rice <rice@fn.lc>
2022-03-11 12:23:38 -0800

    cli: change torchx color to blue and fix for closed stdout

--- torchx/cli/colors.py
+++ torchx/cli/colors.py
@@ -8,13 +8,15 @@
 import sys
 
 # only print colors if outputting directly to a terminal
-if sys.stdout.isatty():
+if not sys.stdout.closed and sys.stdout.isatty():
     GREEN = "\033[32m"
+    BLUE = "\033[34m"
     ORANGE = "\033[38:2:238:76:44m"
     GRAY = "\033[2m"
     ENDC = "\033[0m"
 else:
     GREEN = ""
     ORANGE = ""
+    BLUE = ""
     GRAY = ""
     ENDC = ""
