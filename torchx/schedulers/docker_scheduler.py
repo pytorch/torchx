9d98529e2d3b7a0fcea1cfd95d2e81bfbabbca7f
Priya Ramani <priyaramani@fb.com>
2022-07-07 11:42:27 -0700

    list API for docker

--- torchx/schedulers/docker_scheduler.py
+++ torchx/schedulers/docker_scheduler.py
@@ -448,7 +448,13 @@ class DockerScheduler(Scheduler[DockerOpts], DockerWorkspace):
             return logs
 
     def list(self) -> List[str]:
-        raise NotImplementedError()
+        app_ids = (
+            cntr.labels[LABEL_APP_ID]
+            for cntr in self._docker_client.containers.list(
+                all=True, filters={"label": f"{LABEL_APP_ID}"}
+            )
+        )
+        return list(app_ids)
 
 
 def _to_str(a: Union[str, bytes]) -> str:
