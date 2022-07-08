96c9f3b2cba910d4180eea2778f42851bd4794a5
Priya Ramani <priyaramani@fb.com>
2022-07-08 10:57:50 -0700

    List API - refactor out common logic (#546)
    Summary:
    Move common logic shared across schedulers(kubernetes, docker etc) like creating app_handle from app_id to runner list API from scheduler's list implementation
    
    Pull Request resolved: https://github.com/pytorch/torchx/pull/546
    
    Test Plan: Updated unit tests and ran pytest
    
    Differential Revision: D37673473
    
    Pulled By: priyaramani
    
    fbshipit-source-id: 201626d89f488ff8df96c9cf11299a8dae37358d

--- torchx/schedulers/kubernetes_scheduler.py
+++ torchx/schedulers/kubernetes_scheduler.py
@@ -66,7 +66,6 @@ from torchx.specs.api import (
     runopts,
     VolumeMount,
 )
-from torchx.specs.builders import make_app_handle
 from torchx.workspace.docker_workspace import DockerWorkspace
 from typing_extensions import TypedDict
 
@@ -759,7 +758,7 @@ class KubernetesScheduler(Scheduler[KubernetesOpts], DockerWorkspace):
             return iterator
 
     def list(self) -> List[str]:
-        job_names = []
+        app_ids = []
         namespace = "default"
         resp = self._custom_objects_api().list_namespaced_custom_object(
             group="batch.volcano.sh",
@@ -772,9 +771,8 @@ class KubernetesScheduler(Scheduler[KubernetesOpts], DockerWorkspace):
         for item in items:
             name = item["metadata"]["name"]
             app_id = f"{namespace}:{name}"
-            app_handle = make_app_handle("kubernetes", "default", app_id)
-            job_names.append(app_handle)
-        return job_names
+            app_ids.append(app_id)
+        return app_ids
 
 
 def create_scheduler(session_name: str, **kwargs: Any) -> KubernetesScheduler:
