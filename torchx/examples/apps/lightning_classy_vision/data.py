9412641986c9ba533a920a9eeeca6ac9ec188793
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-02 11:16:02 +0000

    Adding tarfile member sanitization to extractall()

--- torchx/examples/apps/lightning_classy_vision/data.py
+++ torchx/examples/apps/lightning_classy_vision/data.py
@@ -143,7 +143,26 @@ def download_data(remote_path: str, tmpdir: str) -> str:
     data_path = os.path.join(tmpdir, "data")
     print(f"extracting {tar_path} to {data_path}...")
     with tarfile.open(tar_path, mode="r") as f:
-        f.extractall(data_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner) 
+            
+        
+        safe_extract(f, data_path)
 
     return data_path
 
