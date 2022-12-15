f373349b851aac6a619c99c515aea4a7c4b8eb9d
Priya Ramani <priyaramani@fb.com>
2022-12-15 10:47:14 -0800

    cleanup

--- scripts/gcpbatchint.sh
+++ scripts/gcpbatchint.sh
@@ -7,68 +7,24 @@
 
 set -ex
 
-# torchx runopts gcp_batch
+torchx runopts gcp_batch
 
-# APP_ID="$(torchx run --wait --scheduler gcp_batch utils.echo --msg hello)"
-# torchx status "$APP_ID"
-
-# torchx list -s gcp_batch
-# LIST_LINES="$(torchx list -s gcp_batch | grep -c "$APP_ID")"
-# if [ "$LIST_LINES" -ne 1 ]
-# then
-#     echo "expected $APP_ID to be listed"
-#     exit 1
-# fi
-
-# torchx log "$APP_ID"
-# LINES="$(torchx log "$APP_ID" | grep -c 'hello')"
-# if [ "$LINES" -ne 1 ]
-# then
-#     echo "expected 1 log line with 'hello'"
-#     exit 1
-# fi
-
-
-# torchx run --wait --scheduler gcp_batch dist.ddp -j 2x2 --max_retries 3 --script torchx/components/integration_tests/test/dummy_app.py
-
-
-
-JOB="$USER-$(uuidgen)"
-DIR="/tmp/$JOB"
-
-function cleanup {
-  rm  -r "$DIR"
-}
-trap cleanup EXIT
-
-mkdir "$DIR"
-cd "$DIR"
-
-cat <<EOT > main.py
-print("hello world!")
-EOT
-
-RUN_ARGS="--scheduler gcp_batch dist.ddp -j 2x1 --max_retries 3 --script main.py"
-
-
-# shellcheck disable=SC2086
-APP_ID="$(torchx run --wait $RUN_ARGS)"
+APP_ID="$(torchx run --wait --scheduler gcp_batch dist.ddp -j 2x2 --max_retries 3 --script torchx/components/integration_tests/test/dummy_app.py)"
 torchx status "$APP_ID"
-# torchx describe "$APP_ID"
-torchx log "$APP_ID"
-LINES="$(torchx log "$APP_ID" | grep -c 'hello world')"
 
-if [ "$LINES" -ne 2 ]
-then
-    echo "expected 2 log lines"
-    exit 1
-fi
-
-torchx list -s aws_batch
-LIST_LINES="$(torchx list -s aws_batch | grep -c "$APP_ID")"
-
+torchx list -s gcp_batch
+LIST_LINES="$(torchx list -s gcp_batch | grep -c "$APP_ID")"
 if [ "$LIST_LINES" -ne 1 ]
 then
     echo "expected $APP_ID to be listed"
+    exit 1
+fi
+
+torchx log "$APP_ID"
+EXPECTED_MSG="hi from main"
+LINES="$(torchx log "$APP_ID" | grep -c "$EXPECTED_MSG")"
+if [ "$LINES" -ne 2 ]
+then
+    echo "expected 2 log lines with msg $EXPECTED_MSG"
     exit 1
 fi
