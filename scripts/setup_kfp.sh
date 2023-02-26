8f50349ee3e8fb1762e5b464155c08e7bf5c26b9
Tristan Rice <rice@fn.lc>
2023-02-25 21:41:51 -0800

    kfpint: switch to using minikube cluster (#696)

---
+++ scripts/setup_kfp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eux
+
+export PIPELINE_VERSION=1.8.5
+kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION"
+kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io
+kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/env/dev?ref=$PIPELINE_VERSION"
+kubectl apply -f resources/kfp_volcano_role_binding.yaml
