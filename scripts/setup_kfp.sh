e2be5aad9101c1b6c15fb7adb837d56e081f42bb
Tristan Rice <rice@fn.lc>
2023-02-25 21:57:49 -0800

    kfpint: wait for kubeflow to be ready

--- scripts/setup_kfp.sh
+++ scripts/setup_kfp.sh
@@ -7,3 +7,4 @@ kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scop
 kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io
 kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/env/dev?ref=$PIPELINE_VERSION"
 kubectl apply -f resources/kfp_volcano_role_binding.yaml
+kubectl wait --namespace=kubeflow --for condition=available --timeout=10m deployments/metadata-grpc-deployment
