963e082e0c0f38d38878001c9ce5e7addf444772
MichaelClifford <mcliffor@redhat.com>
2023-04-03 11:34:41 -0400

    update ray scheduler to parse more dashboard domain types

--- torchx/schedulers/ray_scheduler.py
+++ torchx/schedulers/ray_scheduler.py
@@ -355,13 +355,13 @@ if _has_ray:
 
         def _parse_app_id(self, app_id: str) -> Tuple[str, str]:
             # find index of '-' in the first :\d+-
-            m = re.search(r":\d+-|.com-", app_id)
+            m = re.search(r":\d+-|.com-|.org-|.net-|.gov-|.io-" , app_id)
             if m:
                 sep = m.span()[1]
-                addr = app_id[: sep - 1]
+                addr = app_id[: sep-1]
                 app_id = app_id[sep:]
                 return addr, app_id
-
+                
             addr, _, app_id = app_id.partition("-")
             return addr, app_id
 
