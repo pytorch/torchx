a9ca56eb424c08ee7baedd322d9b172242c2162f
kiukchung <43595115+kiukchung@users.noreply.github.com>
2023-04-16 17:06:00 -0700

    (torchx/tracking)(bugfix) Allow multiple ranks to properly add artifa… (#727)
    * (torchx/tracking)(bugfix) Allow multiple ranks to properly add artifacts to MLflowTracker
    
    * address PR comments + fix lint - make local_rank() give priority to LOCAL_RANK env var just like rank() and world_size() methods

--- torchx/examples/apps/compute_world_size/module/test/util_test.py
+++ torchx/examples/apps/compute_world_size/module/test/util_test.py
@@ -3,15 +3,20 @@
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
-
+import os
 import random
 import unittest
+from unittest import mock
 
 from omegaconf import DictConfig
 from torchx.examples.apps.compute_world_size.module.util import compute_world_size
 
 
 class UtilTest(unittest.TestCase):
+    # clears env vars like MASTER_PORT, MASTER_ADDR that may be lingering from other tests
+    # when running as a part of a test suite
+    mock.patch.dict(os.environ, clear=True)
+
     def test_compute_world_size(self) -> None:
         cfg = DictConfig(
             content={
