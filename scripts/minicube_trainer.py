0e4793f870fab15bf4e951ef8d0425ef3ff23fac
Yikai Gao <yikai@meta.com>
2024-05-15 10:02:54 -0700

    mini_v15

--- scripts/minicube_trainer.py
+++ scripts/minicube_trainer.py
@@ -54,22 +54,16 @@ def run_job() -> None:
     build = build_and_push_image()
     image = build.torchx_image
     runner = get_runner()
-    # train_app = dist_ddp(
-    #     m="torchx.examples.apps.compute_world_size.main",
-    #     name="ddp-trainer",
-    #     image=image,
-    #     cpu=1,
-    #     j="2x2",
-    #     max_retries=3,
-    #     env={
-    #         "LOGLEVEL": "INFO",
-    #     },
-    # )
     train_app = dist_ddp(
+        m="torchx.examples.apps.compute_world_size.main",
+        name="ddp-trainer",
         image=image,
-        m="torchx.examples.apps.lightning.train",
-        h="GPU_X1",
-        j="2x1",
+        cpu=1,
+        j="2x2",
+        max_retries=3,
+        env={
+            "LOGLEVEL": "INFO",
+        },
     )
     cfg = {
         "namespace": "torchx-dev",
